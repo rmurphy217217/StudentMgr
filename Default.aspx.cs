bfcf7b2336ebd78f580e467c5076e722b88c2d45
rmurphy217217 <77857605+rmurphy217217@users.noreply.github.com>
2021-01-22 14:59:28 -0500

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StdentMgt
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SqlDataSource sqlDS = new SqlDataSource();
+            sqlDS.ConnectionString = "Data Source =.; Initial Catalog = StudentMgt; Integrated Security = True";
+            sqlDS.SelectCommand = "SELECT  Id,[Course Name] FROM [StudentMgt].[dbo].[Course]";
+
+
+            DropDownList1.DataSource = sqlDS;
+            DropDownList1.DataTextField = "Course Name";
+            DropDownList1.DataValueField = "Id";
+
+            DropDownList1.DataBind();
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StdentMgt.Global" Language="C#" %>
